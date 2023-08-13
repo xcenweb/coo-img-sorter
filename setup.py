cd69a37ec8cf9aa8de47b7e6dcabf9c52bbc5124
xcenweb <xcenweb@qq.com>
2023-08-14 00:52:49 +0800

    update'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-with open("requirements.txt", "r", encoding="utf-8") as f:
+with open("requirements.txt", "r", encoding="utf8") as f:
     requires = f.read()
 
 setuptools.setup(
