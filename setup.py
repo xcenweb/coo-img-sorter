65cf49fd6ff17defdd93ffb8a78e40b8b335b034
xcenweb <xcenweb@qq.com>
2023-08-14 00:40:29 +0800

    update

--- setup.py
+++ setup.py
@@ -12,11 +12,16 @@ setuptools.setup(
     license="MIT",
     author="xcenweb",
     author_email="xcenweb@qq.com",
-    url="https://github.com/xcenweb/",
+    url="https://github.com/xcenweb/coo-img-sorter",
 
     description="多种图片分类工具脚手架",
     long_description=long_description,
     long_description_content_type="text/markdown",
+
+    packages=[
+        "coo_imgSorter",
+        "coo_imgSorter.util",
+    ],
 
     python_requires=">=3.8",
     install_requires=requires.splitlines(),
